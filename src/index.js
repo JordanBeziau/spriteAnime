11a7d531be6e7fcb6990f177be23c30648bb08e0
unknown <jordanbeziau@gmail.com>
2017-04-06 12:20:38 +0200

    Déplacement du personnage

--- src/index.js
+++ src/index.js
@@ -1,56 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Fonction créer heures du cadran
-  const createCadran = () => {
-    const cadran = document.getElementById('cadran');
-    let tempNode;
-    // Boucle
-    for (let i = 1; i <= 12; i++) {
-      // Calcul des degrès
-      const deg = 30 * i;
-      // Initialisation de la div
-      tempNode = document.createElement('div');
-      tempNode.className = 'heuresCadran';
-      tempNode.innerHTML = `${i}<br>|`;
-      tempNode.style.transform = `rotate(${deg}deg)`;
-      // Intégration de la div
-      cadran.appendChild(tempNode);
-    }
-  };
-  // Fonction formatage heure
-  const formatHeure = (nombre) => nombre < 10 ? `0${nombre}` : nombre;
-  // Fonction affichage de l'heure
-  const horloge = () => {
-    // ciblage
-    const divHorloge = document.getElementById('affichage'),
-          aiguilleHeu = document.getElementById('aiguilleHeu'),
-          aiguilleMin = document.getElementById('aiguilleMin'),
-          aiguilleSec = document.getElementById('aiguilleSec');
-    // Initialisation de la date
-    const maDate = new Date();
-    let   ampm = '';
-    // Récupération des infos
-    let annee = maDate.getFullYear(),
-        mois = maDate.getMonth(),
-        jour = maDate.getDate(),
-        heures = maDate.getHours(),
-        minutes = maDate.getMinutes(),
-        secondes = maDate.getSeconds();
-    // Calcul de la rotation des aiguilles
-    heures >= 12 ? ampm = 'pm' : ampm = 'am';
-    // Formatage de l'heure
-    heures = formatHeure(heures);
-    minutes = formatHeure(minutes);
-    secondes = formatHeure(secondes);
-    // Affichage
-    divHorloge.innerHTML = `${heures} : ${minutes} : ${secondes} ${ampm}`;
-    // Position des aiguilles
-    aiguilleHeu.style.transform = `rotate(${30 * heures + 0.5 * minutes + 0.5 / 60 * secondes}deg)`;
-    aiguilleMin.style.transform = `rotate(${6 * minutes + 0.1 * secondes}deg)`;
-    aiguilleSec.style.transform = `rotate(${6 * secondes}deg)`;
+  // Ciblage
+  const ken = document.getElementById('ken');
 
+  // Déclaration
+  let posX = 100;
+  let touche = [];
+
+  // Key down
+  document.addEventListener('keydown', (event) => {
+    touche[event.keyCode] = true;
+  });
+
+  // Key up
+  document.addEventListener('keyup', (event) => {
+    touche[event.keyCode] = false;
+  });
+
+  // Move loop
+  function moveLoop () {
+    // Touche gauche
+    if (touche[37]) {
+      ken.style.transform = 'scaleX(1)';
+      if (posX > 0) { posX -= 5; }
+      ken.style.left = `${posX}px`;
+    }
+    // Touche droite
+    if (touche[39]) {
+      ken.style.transform = 'scaleX(-1)';
+      posX += 5;
+      ken.style.left = `${posX}px`;
+    }
+    setTimeout(moveLoop, 15);
   }
 
-  createCadran();
-  setInterval(horloge, 1000);
+  moveLoop();
+
 });
